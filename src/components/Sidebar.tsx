afe664702946e8b29c8eda55a38540a8aaef7a4d
DiegoSouza7 <diego78523@gmail.com>
2021-04-28 20:00:18 -0300

    feat: create page login

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,15 @@
+import { Box, Text } from "@chakra-ui/react";
+
+
+export function Sidebar() {
+  return (
+    <Box
+      w="100%"
+      maxWidth="300px"
+      bg="blue.500"
+      h="100vh"
+    >
+      <Text>MAIN</Text>
+    </Box>
+  )
+}
