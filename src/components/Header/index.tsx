1ab1b6b488f96885d22fad13a42c0695b57c193e
DiegoSouza7 <diego78523@gmail.com>
2021-04-30 14:47:01 -0300

    feat: create header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,66 +1,150 @@
-import { Flex, Icon, Text } from "@chakra-ui/react";
+import { Button, Flex, Icon, Input, Text } from "@chakra-ui/react";
 import {BsSearch} from 'react-icons/bs'
 import {FaRegUser} from 'react-icons/fa'
 import {BiBell} from 'react-icons/bi'
-import {IoMdArrowDropdown} from 'react-icons/io'
+import {IoIosArrowDown, IoMdArrowDropdown} from 'react-icons/io'
 import { useUserContext } from "../../contexts/UserContext";
+import { ImSearch } from "react-icons/im";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type SearchFormData = {
+  search: string;
+}
 
 export function Header () {
+  const { register, handleSubmit } = useForm()
   const {userProfile} = useUserContext()
-  return (
-    <Flex
-      flex={1}
-      h="150px"
-      borderBottom="1px solid #eff0f1"
-      flexDirection="column"
-    >
+
+  const handleSearch: SubmitHandler<SearchFormData> = (data) => {
+    console.log(data)
+  }
+
+  if(userProfile) {
+    return (
       <Flex
-        w="90%"
-        justifyContent="space-between"
-        alignItems="center"
-        h="30px"
-        m="0 auto"
+        flex={1}
+        h="150px"
+        borderBottom="1px solid #eff0f1"
+        flexDirection="column"
       >
-        <Icon as={BsSearch} color="gray.600" cursor="pointer" />
-        <Flex>
-          <Flex w="40px" cursor="pointer">
-            <Icon as={BiBell} color="gray.600" />
-            <Icon as={IoMdArrowDropdown} color="gray.600" />
+        <Flex
+          w="90%"
+          justifyContent="space-between"
+          alignItems="center"
+          h="30px"
+          m="0 auto"
+        >
+          <Icon as={BsSearch} color="gray.600" cursor="pointer" />
+          <Flex>
+            <Flex w="40px" cursor="pointer">
+              <Icon as={BiBell} color="gray.600" />
+              <Icon as={IoMdArrowDropdown} color="gray.600" />
+            </Flex>
+            <Flex w="40px" cursor="pointer">
+              <Icon as={FaRegUser} color="gray.600" />
+              <Icon as={IoMdArrowDropdown} color="gray.600" />
+            </Flex>
           </Flex>
-          <Flex w="40px" cursor="pointer">
-            <Icon as={FaRegUser} color="gray.600" />
-            <Icon as={IoMdArrowDropdown} color="gray.600" />
+        </Flex>
+        <Flex
+          w="90%"
+          h="30px"
+          m="0 auto"
+          mt="20px"
+        >
+          <Text
+            color="gray.600"
+            fontSize="2xl"
+            fontWeight={700}
+          >
+            Repositories
+          </Text>
+        </Flex>
+        <Flex
+          w="90%"
+          h="30px"
+          m="0 auto"
+          mt="20px"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Text
+            color="gray.600"
+            fontSize="lg"
+            fontWeight={700}
+          >
+            {userProfile.public_repos} Repositories has created so far
+          </Text>
+
+          <Flex
+            as="form"
+            justifyContent="space-between"
+            alignItems="center"
+            onSubmit={handleSubmit(handleSearch)}
+          >
+            <Button type="submit" bg="transparent" border="none" _hover={{
+              bg: "transparent", border: "none"
+            }}>
+              <Icon
+                as={ImSearch}
+                color="gray.600"
+                fontSize="20"
+              />
+            </Button>
+            <Input
+              name="search"
+              placeholder="SEARCH"
+              color="gray.600"
+              border="none"
+              {...register('search')}
+            />
+          </Flex>
+          
+          <Flex
+            justifyContent="space-between"
+            alignItems="center"
+            w="300px"
+          >
+            <Flex
+              justifyContent="space-evenly"
+            >
+              <Text
+                color="gray.600"
+                fontWeight={700}
+              >
+                TYPE
+              </Text>
+              <Icon as={IoIosArrowDown} color="gray.600" ml="5px" />
+            </Flex>
+            <Flex
+              justifyContent="space-evenly"
+            >
+              <Text
+                color="gray.600"
+                fontWeight={700}
+              >
+                LANGUAGE
+              </Text>
+              <Icon as={IoIosArrowDown} color="gray.600" ml="5px" />
+            </Flex>
+            <Flex
+              justifyContent="space-evenly"
+            >
+              <Text
+                color="gray.600"
+                fontWeight={700}
+              >
+                DATE
+              </Text>
+              <Icon as={IoIosArrowDown} color="gray.600" ml="5px" />
+            </Flex>
           </Flex>
         </Flex>
       </Flex>
-      <Flex
-        w="90%"
-        h="30px"
-        m="0 auto"
-        mt="20px"
-      >
-        <Text
-          color="gray.600"
-          fontSize="2xl"
-          fontWeight={700}
-        >
-          Repositories
-        </Text>
-      </Flex>
-      <Flex
-        w="90%"
-        h="30px"
-        m="0 auto"
-        mt="20px"
-      >
-        <Text
-          color="gray.600"
-          fontSize="lg"
-          fontWeight={700}
-        >
-          {userProfile.public_repos} Repositories has created so far
-        </Text>
-      </Flex>
-    </Flex>
+    )
+  }
+
+  return (
+    <h1>Carregando</h1>
   )
 }
